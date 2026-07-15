83e5435201d59605e1798d28edbf191042b1479b
henriqueeeloisa333-pixel <henriqueeeloisa333@gmail.com>
2026-07-15 12:56:31 -0400

    Update funcoes.py atu

--- funcoes.py
+++ funcoes.py
@@ -3,9 +3,4 @@ def definir_cor(nota):
     if nota <= 1.9: return "red"
     if nota <= 3.9: return "orange"
     if nota <= 4.5: return "green"
-    return "blue"def definir_cor(nota):
-    if nota == 0: return "gray"
-    if nota <= 1.9: return "red"
-    if nota <= 3.9: return "orange"
-    if nota <= 4.5: return "green"
     return "blue"
